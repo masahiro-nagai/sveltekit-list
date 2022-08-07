fa085aab42fe51c3798070147eecb832f90a15f4
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-08-07 22:54:51 +0900

    component props

---
+++ src/lib/User.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let user;
+    export let i;
+</script>
+<div  class="flex m-5 bg-red border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center">
+    <p class="w-4">{i + 1}</p>
+    <img class="rounded-full w-14 h-14" src={user.image} alt={user.name} />
+    <p class="mx-4 w-20">{user.name}</p>
+    <p class="mx-4 w-auto">{user.email}</p>
+</div>
---
+++ src/lib/Users.svelte
@@ -0,0 +1,38 @@
+<script>
+    import user1 from "../assets/images/user1.png";
+    import user2 from "../assets/images/user2.png";
+    import user3 from "../assets/images/user3.png";
+    import User from "./User.svelte"
+    let users = [
+        {
+            id:1,
+            image: user1,
+            name: "永井",
+            email: "nagai.hamaya@gmail.com",
+        },
+        {
+            id:2,
+            image: user2,
+            name: "まさ",
+            email: "masa.hamaya@gmail.com",
+        },
+         {
+            id:3,
+            image: user3,
+            name: "anonymous",
+            email: "anonymous.hamaya@gmail.com",
+        },
+    ];
+    const getUsers = () =>{
+        return users;
+    };
+</script>
+
+<div>
+    <h1 class="text-2xl text-center mt-10">ユーザーリスト</h1>
+    {#each getUsers() as user,i(user.id)}
+    <User {user} {i}/>
+    {:else}
+        <p>ユーザーはいません</p>
+    {/each}
+</div>
